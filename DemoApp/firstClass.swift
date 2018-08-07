a9653746730a99b895b700531f6e07ceed5bdc58
Manish <manish@goingit.com.au>
2018-08-07 12:12:58 +0530

    add class file

---
+++ DemoApp/firstClass.swift
@@ -0,0 +1,14 @@
+//
+//  firstClass.swift
+//  DemoApp
+//
+//  Created by Goingit on 07/08/18.
+//  Copyright © 2018 Goingit. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TopClass{
+
+}
